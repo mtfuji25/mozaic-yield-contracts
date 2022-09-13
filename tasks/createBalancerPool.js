f2cafbe7168dd56212409a49e42bb906f8a082ac
tiger0105 <zh.jing0105@outlook.com>
2022-09-14 05:15:08 +0700

    createBalancerPool  task (not completed)

--- tasks/createBalancerPool.js
+++ tasks/createBalancerPool.js
@@ -1,7 +1,7 @@
 const { POOLS } = require("@layerzerolabs/sg-sdk")
 const { getEndpointId } = require("../utils/network")
 
-task("createPools", "create the pools for the network").setAction(async (taskArgs) => {
+task("createBalancerPool", "create the pools for the network").setAction(async (taskArgs) => {
     let router = await ethers.getContract("Router")
     let pools = POOLS[hre.network.name]
     // console.log(pools)
@@ -12,16 +12,19 @@ task("createPools", "create the pools for the network").setAction(async (taskArg
     // }
 
     // make sure the token exists
-    let tx
+    
     for (let poolId in pools) {
         console.log(pools[poolId].info)
         console.log(`poolId: ${poolId}`)
         console.table(pools[poolId].chainPaths)
+
         let sharedDecimals = pools[poolId].info.sharedDecimals
         let address = pools[poolId].info.address
 
         let Token = await ethers.getContractFactory("MockToken")
         let token = await Token.attach(address)
+        let Token2 = await ethers.getContractFactory("MockToken")
+        let token2 = await Token2.attach(address)
         let name = await token.name()
         let symbol = await token.symbol()
         let decimals = await token.decimals()
@@ -32,19 +35,16 @@ task("createPools", "create the pools for the network").setAction(async (taskArg
         const factory = await ethers.getContract("Factory")
 
         try {
-            tx = await (
-                await router.createPool(
-                    poolId,
-                    token.address,
-                    sharedDecimals, // Stargate pairs shared decimals across chains
-                    decimals, // the tokens real decimals
-                    poolName,
-                    poolSymbol
-                )
-            ).wait()
-            let poolAddr = await factory.getPool(poolId)
-            console.log(`[${getEndpointId()}] createPool | name:${name} symbol:${symbol} decimals:${decimals} address:${token.address}`)
-            console.log(`- tx: ${tx.transactionHash} | pool.address: ${poolAddr} name: ${poolName} symbol: ${poolSymbol}`)
+            let poolAddr = await router.createBalancerPool(
+                    poolId, 
+                    poolName, 
+                    poolSymbol, 
+                    [token.address, token2.address], 
+                    [ethers.utils.parseEther("0.5"), ethers.utils.parseEther("0.5")], 
+                    [ZERO_ADDRESS, ZERO_ADDRESS], 
+                    ethers.utils.parseEther("0.001"))
+            
+            console.log(`[${getEndpointId()}] createBalancerPool | name:${poolName} symbol:${poolSymbol} pool address: ${poolAddr}`)
         } catch (e) {
             if (e.error.message.includes("Stargate: Pool already created")) {
                 let poolAddr = await factory.getPool(poolId)
